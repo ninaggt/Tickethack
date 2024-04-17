c0bc931c4b66e064743e183516727e765172ae11
Hanitsoa <hanitsoa_2004@yahoo.fr>
2024-04-17 20:58:23 +0200

    front

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,4 @@
 
-
 const btnSearch = document.querySelector(".btn");
 const date = document.querySelector("#date");
 const ticketthat = document.querySelector("#logo");
@@ -7,26 +6,12 @@ const cart = document.querySelector("#cart");
 const booking = document.querySelector("#bookings");
 const voyage = document.querySelector(".voyage");
 
-// const tableau = [{"departure":"Paris",
-//               "arrival":"Lyon",
-//               "date":{"$date":"2024-04-16T08:43:53.356Z"},
-//               "price":148},
-//              {"departure":"Bruxelles",
-//               "arrival":"Marseille",
-//               "date":{"$date":"2024-04-16T08:45:49.731Z"},
-//               "price":140},
-//              {"departure":"Paris","arrival":"Lyon","date":{"$date":"2024-04-16T08:59:20.204Z"},"price":70},{"departure":"Marseille","arrival":"Bruxelles","date":{"$date":"2024-04-16T09:03:56.627Z"},"price":87}]
+departure.addEventListener("click", function() {})
+arrival.addEventListener("click", function() {})
+date.addEventListener("click", function() {})
 
-departure.addEventListener("click", function() {
-    
-})
-arrival.addEventListener("click", function() {
-
-})
-date.addEventListener("click", function() {
-
-})
 btnSearch.addEventListener("click", function() {
+
     const departure = document.querySelector("#departure");
     const arrival = document.querySelector("#arrival");
 
@@ -35,38 +20,58 @@ btnSearch.addEventListener("click", function() {
         arrival: arrival.value,
         date: date.value
     };
-    console.log(departure.value, arrival.value,date.value);
-    
+
+    if (!departure.value && !arrival.value){
+        voyage.innerHTML = 
+        `<div>
+            <div class="image">
+                <img src="./images/notfound.png" alt="notfound">
+            </div>
+            <div class="separation"></div>
+            <p>no trip found.</p>
+        </div>`
+    } else {
+            
     fetch('http://localhost:3000/trips', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify( trajet ),
 	}).then(response => response.json())
 	  .then(data => {
-            console.log(data.voyage);
+          
             voyage.innerHTML ="";
     for (const objet of data.voyage) {
         
         const objetKey = Object.values(objet);
-        console.log(objetKey);
+      
+     
         voyage.innerHTML += 
         `<div id="allee">
             <ul>
-                <li><span>${objetKey[1]} > ${objetKey[2]}</span><span> ${objetKey[3]}<span> ${objetKey[4]}</span> <button class="voir">Add</button></li>
+                <li id="${objetKey[0]}">${objetKey[1]} > ${objetKey[2]}${objetKey[3]}${objetKey[4]} <button class="voir">Add</button></li>
             </ul>
         </div>`
-        
+       
+       
+        for (let i = 0; i < document.querySelectorAll('.voir').length; i++) {
+            document.querySelectorAll('.voir')[i].addEventListener('click',
+              function () {
+                let idElement = this.parentNode.id;
+                
+                fetch(`http://localhost:3000/tickethack/cart/`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify( {id:idElement} ),
+                }).then(response => response.json())
+                .then(data => {
+                console.log(data);
+                })
+                window.location.assign("./cartF.html");            
+            });
+        }
     }
-    const liLists = document.querySelectorAll("li");
-    for (let i = 0; i < document.querySelectorAll('.voir').length; i++) {
-        document.querySelectorAll('.voir')[i].addEventListener('click',
-          function () {
-            this.style.color = "red";
-            //Appeler cartF.js 
-          }
-        );
+})
     }
-    })
     
-   
+
 })
